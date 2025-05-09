04b878279793ae26845002f10764a622459379ed
gnqHuy <quanghuyy150604@gmail.com>
2025-05-09 23:12:19 +0700

    feat: notification

--- frontend/components/Notification/NotificationImage.tsx
+++ frontend/components/Notification/NotificationImage.tsx
@@ -3,23 +3,61 @@ import { View, Image, ImageSourcePropType } from 'react-native';
 import { FontAwesome, FontAwesome6 } from '@expo/vector-icons';
 
 interface NotificationImageProps {
-  image: ImageSourcePropType;
-  iconName: keyof typeof FontAwesome.glyphMap;
-  iconColor?: string;
-  iconSize?: number;
+  image: string;
+  type: string;
 }
 
 const NotificationImage: React.FC<NotificationImageProps> = ({
   image,
-  iconName,
-  iconColor = '#000',
-  iconSize = 20,
+  type,
 }) => {
+  if (image == null) {
+    image = "https://lumi-learn.s3.ap-southeast-2.amazonaws.com/course/0750cca2-4efd-4089-9ec0-b4cd698833ae";
+  }
+  console.log(image);
   return (
-    <View className="w-[57px] h-[57px] my-auto overflow-hidden relative">
-      <Image source={image} className="w-[95%] h-[95%]" resizeMode="cover" />
-      <View className="absolute bottom-[0] right-[0]">
-        <FontAwesome6 name={iconName} size={iconSize} color={iconColor} />
+    <View className="w-[64px] h-[64px] my-auto overflow-hidden relative">
+      <Image source={require("../../assets/images/lumiLearnLogo.png")} className="w-[100%] h-[100%] rounded-full bg-blue-300" resizeMode="contain" />
+      <View className="absolute bottom-[0] right-[0] bg-[#b2e4b3] rounded-full p-[2px] border border-black">
+        {type == 'FlashCardSet' && 
+          <Image
+            source={require('../../assets/images/flash-card.png')}
+            style={{ width: 16, height: 16 }}
+            resizeMode="contain"
+          />
+        }
+
+        {type == 'Quiz' && 
+          <Image
+            source={require('../../assets/images/quiz1.png')}
+            style={{ width: 16, height: 16,}}
+            resizeMode="contain"
+          />
+        }
+
+        {type == 'StudentEnrolled' && 
+          <Image
+            source={require('../../assets/images/add-friend.png')}
+            style={{ width: 16, height: 16 }}
+            resizeMode="contain"
+          />
+        }
+
+        {type == 'StudentSubmitQuiz' && 
+          <Image
+            source={require('../../assets/images/submit.png')}
+            style={{ width: 16, height: 16 }}
+            resizeMode="contain"
+          />
+        }
+
+        {type == 'default' && 
+          <Image
+            source={require('../../assets/images/flash-card.png')}
+            style={{ width: 16, height: 16 }}
+            resizeMode="contain"
+          />
+        }   
       </View>
     </View>
   );
